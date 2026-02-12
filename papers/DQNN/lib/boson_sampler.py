e8e92166c2d88cf158dd1ef19be5d0fa06e1f2a6
Cassandre Notton <cassandrenotton78@gmail.com>
2026-02-12 16:03:36 -0500

    Last ruff fix

--- papers/DQNN/lib/boson_sampler.py
+++ papers/DQNN/lib/boson_sampler.py
@@ -172,22 +172,20 @@ class BosonSampler:
         width = len(str(self.nb_parameters - 1))
         parameters = [f"phi{i:0{width}d}" for i in range(self.num_effective_params)]
 
-        quantum_layer_kwargs = dict(
-            input_size=0,
-            n_photons=self.n,
-            circuit=circuit,
-            input_state=input_state,
-            trainable_parameters=parameters,
-        )
+        quantum_layer_kwargs = {
+            "input_size": 0,
+            "n_photons": self.n,
+            "circuit": circuit,
+            "input_state": input_state,
+            "trainable_parameters": parameters,
+        }
 
         # Merlin >=0.3 deprecates passing computation_space directly.
         if hasattr(ML, "MeasurementStrategy") and hasattr(
             ML.MeasurementStrategy, "probs"
         ):
-            quantum_layer_kwargs["measurement_strategy"] = (
-                ML.MeasurementStrategy.probs(
-                    computation_space=ML.ComputationSpace.UNBUNCHED
-                )
+            quantum_layer_kwargs["measurement_strategy"] = ML.MeasurementStrategy.probs(
+                computation_space=ML.ComputationSpace.UNBUNCHED
             )
         else:
             quantum_layer_kwargs["computation_space"] = ML.ComputationSpace.UNBUNCHED
